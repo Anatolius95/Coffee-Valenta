6d88449e04c81a939ed306e36999231f7743e988
Anatolius95 <valenta.tomas95@gmail.com>
2016-07-14 16:24:08 +0800

    Desing

--- app/src/main/java/cz/uhk/coffee_valenta/CoffeeOrder.java
+++ app/src/main/java/cz/uhk/coffee_valenta/CoffeeOrder.java
@@ -3,6 +3,7 @@
     import android.os.Bundle;
     import android.support.v7.app.ActionBarActivity;
     import android.view.View;
+    import android.widget.CheckBox;
     import android.widget.TextView;
 
     import java.text.NumberFormat;
@@ -10,6 +11,7 @@
     public class CoffeeOrder extends ActionBarActivity {
 
         int NumberOfCoffees=0;
+        int CoffeePrice=12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +21,66 @@
         public void submitOrder(View view) {
             TextView priceTextView = (TextView) findViewById(
                     R.id.price_text_view);
-            priceTextView.setText("Order received");
+
+            TextView priceHeadLineTextView = (TextView) findViewById(
+                    R.id.PriceHeadLine_text_view);
+
+            CheckBox milkCheck = (CheckBox) findViewById(
+                    R.id.milk);
+
+            CheckBox sugarCheck = (CheckBox) findViewById(
+                    R.id.sugar);
+
+            boolean milkB=milkCheck.isChecked();
+            boolean sugarB=sugarCheck.isChecked();
+
+            String milk = "";
+            String sugar = "";
+
+            if (milkB) {
+                milk = "Yes";
+                displayPrice(NumberOfCoffees * CoffeePrice);
+                }else{
+                    milk = "No";
+                }
+
+            if (sugarB) {
+                sugar = "Yes";
+                displayPrice(NumberOfCoffees * CoffeePrice);
+                }else{
+                    sugar = "No";
+                }
+
+            priceHeadLineTextView.setText("SUMMARY");
+            priceTextView.setText("Amount: " + NumberOfCoffees + "\n Price: " + NumberOfCoffees * CoffeePrice + "$ \n Milk: " + milk + "\n Sugar: " + sugar);
+            priceTextView.setTextSize(20);
         }
     public void submitOrderPlus(View view) {
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view);
+        TextView priceHeadLineTextView = (TextView) findViewById(
+                R.id.PriceHeadLine_text_view);
+
         NumberOfCoffees = NumberOfCoffees + 1;
         display(NumberOfCoffees);
-        displayPrice(NumberOfCoffees * 12);
+        displayPrice(NumberOfCoffees * CoffeePrice);
+        priceTextView.setTextSize(40);
+        priceHeadLineTextView.setText("PRICE");
     }
         public void submitOrderMinus(View view) {
+            TextView priceTextView = (TextView) findViewById(
+                    R.id.price_text_view);
+            TextView priceHeadLineTextView = (TextView) findViewById(
+                    R.id.PriceHeadLine_text_view);
+
             if (NumberOfCoffees < 1) {
                 NumberOfCoffees = NumberOfCoffees + 1;
             }
-
             NumberOfCoffees = NumberOfCoffees - 1;
             display(NumberOfCoffees);
-            displayPrice(NumberOfCoffees * 12);
+            displayPrice(NumberOfCoffees * CoffeePrice);
+            priceTextView.setTextSize(40);
+            priceHeadLineTextView.setText("PRICE");
         }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
