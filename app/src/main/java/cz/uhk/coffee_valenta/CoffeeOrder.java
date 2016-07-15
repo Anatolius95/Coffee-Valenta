4c6a1f98bc233032119ebc9eaff17883c410153f
Anatolius95 <valenta.tomas95@gmail.com>
2016-07-15 09:55:01 +0800

    Desing

--- app/src/main/java/cz/uhk/coffee_valenta/CoffeeOrder.java
+++ app/src/main/java/cz/uhk/coffee_valenta/CoffeeOrder.java
@@ -1,95 +1,159 @@
-    package cz.uhk.coffee_valenta;
+package cz.uhk.coffee_valenta;
 
-    import android.os.Bundle;
-    import android.support.v7.app.ActionBarActivity;
-    import android.view.View;
-    import android.widget.CheckBox;
-    import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-    import java.text.NumberFormat;
+import java.text.NumberFormat;
 
-    public class CoffeeOrder extends ActionBarActivity {
+public class CoffeeOrder extends ActionBarActivity {
 
-        int NumberOfCoffees=0;
-        int CoffeePrice=12;
+    int NumberOfCoffees = 0;
+    int CoffeePrice = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee_order);
     }
-        public void submitOrder(View view) {
-            TextView priceTextView = (TextView) findViewById(
-                    R.id.price_text_view);
 
-            TextView priceHeadLineTextView = (TextView) findViewById(
-                    R.id.PriceHeadLine_text_view);
+    public void submitOrder(View view) {
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view);
 
-            CheckBox milkCheck = (CheckBox) findViewById(
-                    R.id.milk);
+        TextView priceHeadLineTextView = (TextView) findViewById(
+                R.id.PriceHeadLine_text_view);
 
-            CheckBox sugarCheck = (CheckBox) findViewById(
-                    R.id.sugar);
+        CheckBox milkCheck = (CheckBox) findViewById(
+                R.id.milk);
 
-            boolean milkB=milkCheck.isChecked();
-            boolean sugarB=sugarCheck.isChecked();
+        CheckBox sugarCheck = (CheckBox) findViewById(
+                R.id.sugar);
 
-            String milk = "";
-            String sugar = "";
+        boolean milkB = milkCheck.isChecked();
+        boolean sugarB = sugarCheck.isChecked();
 
-            if (milkB) {
-                milk = "Yes";
-                displayPrice(NumberOfCoffees * CoffeePrice);
-                }else{
-                    milk = "No";
-                }
+        String milk = "";
+        String sugar = "";
 
-            if (sugarB) {
-                sugar = "Yes";
-                displayPrice(NumberOfCoffees * CoffeePrice);
-                }else{
-                    sugar = "No";
-                }
-
-            priceHeadLineTextView.setText("SUMMARY");
-            priceTextView.setText("Amount: " + NumberOfCoffees + "\n Price: " + NumberOfCoffees * CoffeePrice + "$ \n Milk: " + milk + "\n Sugar: " + sugar);
-            priceTextView.setTextSize(20);
+        if (milkB) {
+            milk = "Yes";
+            displayPrice(NumberOfCoffees * CoffeePrice);
+        } else {
+            milk = "No";
         }
+
+        if (sugarB) {
+            sugar = "Yes";
+            displayPrice(NumberOfCoffees * CoffeePrice);
+        } else {
+            sugar = "No";
+        }
+
+        priceHeadLineTextView.setText("SUMMARY");
+        priceTextView.setText("Amount: " + NumberOfCoffees + "\n Price: " + NumberOfCoffees * CoffeePrice + "$ \n Milk: " + milk + "\n Sugar: " + sugar);
+        priceTextView.setTextSize(25);
+    }
+
     public void submitOrderPlus(View view) {
         TextView priceTextView = (TextView) findViewById(
                 R.id.price_text_view);
+
         TextView priceHeadLineTextView = (TextView) findViewById(
                 R.id.PriceHeadLine_text_view);
 
         NumberOfCoffees = NumberOfCoffees + 1;
+
         display(NumberOfCoffees);
         displayPrice(NumberOfCoffees * CoffeePrice);
+
         priceTextView.setTextSize(40);
         priceHeadLineTextView.setText("PRICE");
     }
-        public void submitOrderMinus(View view) {
-            TextView priceTextView = (TextView) findViewById(
-                    R.id.price_text_view);
-            TextView priceHeadLineTextView = (TextView) findViewById(
-                    R.id.PriceHeadLine_text_view);
 
-            if (NumberOfCoffees < 1) {
-                NumberOfCoffees = NumberOfCoffees + 1;
-            }
-            NumberOfCoffees = NumberOfCoffees - 1;
-            display(NumberOfCoffees);
-            displayPrice(NumberOfCoffees * CoffeePrice);
-            priceTextView.setTextSize(40);
-            priceHeadLineTextView.setText("PRICE");
+    public void submitOrderMinus(View view) {
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view);
+
+        TextView priceHeadLineTextView = (TextView) findViewById(
+                R.id.PriceHeadLine_text_view);
+
+        if (NumberOfCoffees < 1) {
+            NumberOfCoffees = NumberOfCoffees + 1;
         }
+
+        NumberOfCoffees = NumberOfCoffees - 1;
+
+        display(NumberOfCoffees);
+        displayPrice(NumberOfCoffees * CoffeePrice);
+
+        priceTextView.setTextSize(40);
+        priceHeadLineTextView.setText("PRICE");
+    }
+
+    public void reset(View view) {
+        NumberOfCoffees = 0;
+        CoffeePrice = 12;
+
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view);
+
+        TextView priceHeadLineTextView = (TextView) findViewById(
+                R.id.PriceHeadLine_text_view);
+
+        priceTextView.setTextSize(40);
+        priceHeadLineTextView.setText("PRICE");
+
+        CheckBox milkCheck = (CheckBox) findViewById(
+                R.id.milk);
+
+        CheckBox sugarCheck = (CheckBox) findViewById(
+                R.id.sugar);
+
+        milkCheck.setChecked(false);
+        sugarCheck.setChecked(false);
+
+        display(NumberOfCoffees);
+        displayPrice(NumberOfCoffees * CoffeePrice);
+    }
+
+    public void priceIncreaseM(View view) {
+        CheckBox milkCheck = (CheckBox) findViewById(
+                R.id.milk);
+
+        if (milkCheck.isChecked()) {
+            CoffeePrice = CoffeePrice + 2;
+            displayPrice(NumberOfCoffees * CoffeePrice);
+        } else {
+            CoffeePrice = CoffeePrice - 2;
+            displayPrice(NumberOfCoffees * CoffeePrice);
+        }
+    }
+
+    public void priceIncreaseS(View view) {
+        CheckBox sugarCheck = (CheckBox) findViewById(
+                R.id.sugar);
+
+        if (sugarCheck.isChecked()) {
+            CoffeePrice = CoffeePrice + 2;
+            displayPrice(NumberOfCoffees * CoffeePrice);
+        } else {
+            CoffeePrice = CoffeePrice - 2;
+            displayPrice(NumberOfCoffees * CoffeePrice);
+        }
+    }
+
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
+
         quantityTextView.setText("" + number);
     }
 
-        private void displayPrice(int number) {
-            TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-            priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-        }
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
 }
